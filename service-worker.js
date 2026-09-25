74426c6ac56da849c1dd8b414204523cbd2ddf8d
El Mokhtar Bensaid <b.mokhtar20@gmail.com>
2026-09-25 08:29:11 -0400

    Add service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const C="pbm-v1";const A=["./","./index.html","./style.css","./app.js","./manifest.webmanifest"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));self.addEventListener("fetch",e=>e.request.method==="GET"&&e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
